5151515748acf4ec3f078a7c239e9d3bde834fb6
kobe <1329989713@qq.com>
2018-08-08 23:23:44 +0800

    愊

--- src/com/kobe/io0/Client.java
+++ src/com/kobe/io0/Client.java
@@ -70,7 +70,7 @@ public class Client {
 
 
 
-    // 接收消息的方法。
+    // 接收消息的方法。kk
     public static void getMessage() throws Exception {
         InputStream inputStream = socket.getInputStream();
         InputStreamReader isr = new InputStreamReader(inputStream);
